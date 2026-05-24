ede6e3d88dfa60ebbe9f8b8472caab8e76646a1e
bibikashrt <bibikashrestha17@gmail.com>
2026-05-24 09:35:38 +0545

    Learned OOP 4 pillars and implementaion.

---
+++ Day5/Polymorphism.java
@@ -0,0 +1,23 @@
+
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class Polymorphism {
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println(c.add(10, 20));
+
+        System.out.println(c.add(10, 20, 30));
+    }
+}
