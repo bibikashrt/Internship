9de581b8d9d8ced47747c1ddc3900d1403be8175
bibikashrt <bibikashrestha17@gmail.com>
2026-05-31 10:25:10 +0545

    Add Java programs for conditional statements

---
+++ Day9/ReverseNumber.java
@@ -0,0 +1,16 @@
+
+class ReverseNumber {
+
+    int reverse(int n) {
+        int rev = 0;
+        while (n != 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseNumber().reverse(1234));
+    }
+}
